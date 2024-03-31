5ad04d227966cb97ee45940d733b22ea44ac332a
Manojkumar1709 <mm2040922@gmail.com>
2024-03-31 22:31:45 +0530

    This is an unit test case using Python

---
+++ test.py
@@ -0,0 +1,26 @@
+import unittest
+from selenium import webdriver
+
+class TestWebsiteLoading(unittest.TestCase):
+    def setUp(self):
+        self.driver = webdriver.Chrome()  
+        self.driver.implicitly_wait(10)
+
+    def test_website_load(self):
+        print("Loading the website...")
+        try:
+            self.driver.get("https://atg.world")
+            print("Website loaded successfully")
+            result = "Test case is passed"
+        except Exception as e:
+            print("Failed to load the website")
+            result = "Test case is failed"
+        print(result)
+
+
+
+    def tearDown(self):
+        self.driver.quit()
+
+if __name__ == "__main__":
+    unittest.main()
